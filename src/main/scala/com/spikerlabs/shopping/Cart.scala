e1ed5cf422ede3730926cc5ab9b7a4e716da2166
Arturas Smorgun <arturas@spikerlabs.com>
2017-05-09 13:50:06 +0100

    Fix formatting

--- src/main/scala/com/spikerlabs/shopping/Cart.scala
+++ src/main/scala/com/spikerlabs/shopping/Cart.scala
@@ -1,12 +1,15 @@
 package com.spikerlabs.shopping
+
 trait SomeCart {
   def products: List[Product]
   def total: Int
   def formattedTotal: String = f"£${total / 100.0}%.2f"
 }
+
 case class Cart(products: List[Product]) extends SomeCart {
   def total: Int = products.foldLeft(0)(_ + _.price)
 }
+
 object Cart {
   def fromCodes(codes: List[String]): Cart = {
     val products = codes.map(Product.scan).filter(_.nonEmpty).flatten
--- src/main/scala/com/spikerlabs/shopping/Products.scala
+++ src/main/scala/com/spikerlabs/shopping/Products.scala
@@ -1,12 +1,15 @@
 package com.spikerlabs.shopping
+
 sealed trait Product {
   def code: String
   def price: Int
 }
+
 final case class Apple() extends Product {
   val code = "Apple"
   val price = 60
 }
+
 final case class Orange() extends Product {
   val code = "Orange"
   val price = 25
